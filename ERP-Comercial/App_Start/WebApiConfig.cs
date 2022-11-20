5c6276ac92fbfee6cb62a03e9fa0d5df142df1ed
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2022-11-19 22:17:40 -0600

    Se subio el archivo principal

---
+++ ERP-Comercial/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ERP_Comercial
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
